1820796100a5d6dede22b6af51362b583f184c91
Johan <johan@romantech.net>
2021-05-30 19:03:25 +0900

    revise file name

--- Level 1/level01_42576_완주하지-못한-선수.js
+++ Level 1/level01_42576_완주하지-못한-선수.js
@@ -2,6 +2,27 @@
 // completion 완주한 선수들의 이름이 담긴 배열
 // 요구사항 : 완주하지 못한 선수의 이름 1개 return
 
+// * 내가 푼 코드 50%
+// function solution(participant, completion) {
+//   participant.sort();
+//   completion.sort();
+
+//   return participant.filter(el => {
+//     let failed = true;
+
+//     for (let i = 0; i < completion.length; i += 1) {
+//       if (completion[i] === el) {
+//         completion.splice(i, 1);
+//         failed = false;
+//         break;
+//       }
+//     }
+
+//     return failed;
+//   })[0];
+// }
+
+// * 레퍼런스
 function solution(participant, completion) {
   participant.sort();
   completion.sort();
