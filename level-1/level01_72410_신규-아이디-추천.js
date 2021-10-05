8069796622d49f36f68e832564bfd4c637927646
Johan <johan@romantech.net>
2021-10-05 16:01:35 +0900

    Update level01_72410

--- level-1/level01_72410_신규-아이디-추천.js
+++ level-1/level01_72410_신규-아이디-추천.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-param-reassign */
 /* eslint-disable camelcase */
 /* cSpell:disable */
 
@@ -11,11 +12,49 @@
 // 7단계 : 길이가 2자 이하면, 문자 길이가 3이 될 때까지 마지막 문자 반복해서 붙이기
 
 function solution(new_id) {
-  const answer = '';
-  return answer;
+  if (new_id.length === 0) {
+    return 'aaa';
+  }
+
+  const stepOne = new_id.toLowerCase();
+  const stepTwo = stepOne.replace(/[^a-z0-9\-_.]/gi, '');
+  const stepThree = stepTwo.replace(/\.{2,}/gi, '.');
+
+  const removePeriod = str => {
+    if (str[0] === '.') {
+      str = str.slice(1);
+    }
+    if (str[str.length - 1] === '.') {
+      str = str.slice(0, -1);
+    }
+
+    return str;
+  };
+
+  const stepFour = removePeriod(stepThree);
+  const stepFive = stepFour === '' ? 'a' : stepFour;
+  let stepSix = stepFive.slice(0, 15);
+  stepSix = removePeriod(stepSix);
+
+  const checkLength = str => {
+    if (str.length < 3) {
+      if (str.length === 0) {
+        return 'aaa';
+      }
+      if (str.length === 1) {
+        return str + str[0] + str[0];
+      }
+      return str + str[1];
+    }
+    return str;
+  };
+
+  return checkLength(stepSix);
 }
 
 const test1 = {
   new_id: '...!@BaT#*..y.abcdefghijklm',
   result: 'bat.y.abcdefghi',
 };
+
+solution('=.=');
