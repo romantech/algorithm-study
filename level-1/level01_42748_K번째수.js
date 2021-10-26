d42fdfdfb3c68147332098b202debf474ef85aec
Johan <johan@romantech.net>
2021-10-26 21:34:14 +0900

    :memo: Update review "level01_42748_K번째수"

--- level-1/level01_42748_K번째수.js
+++ level-1/level01_42748_K번째수.js
@@ -13,6 +13,13 @@ function solution(array, commands) {
     const end = cur[1];
     const seq = cur[2] - 1;
     return acc.concat(array.slice(start, end).sort((a, b) => a - b)[seq]);
+    /**
+     * sort() 메소드에서 compare func(콜백)를 넘기지 않으면 아스키코드를 기준으로 정렬한다(사전식 정렬?)
+     * [1, 8, 88, 9, 10]을 정렬하면 [1, 10, 8, 88, 9]가 됨
+     * 1의 아스키코드는 49, 0의 아스키코드는 48, 8의 아스키코드느 56 (10진법 기준)
+     * 8의 아스키코드 56이고 10의 앞글자 1의 아스키코드가 49이므로 10보다 8이 크다가 판단한다
+     * compare func를 사용하면 정수 크기대로 정렬할 수 있다
+     */
   }, []);
 }
 
