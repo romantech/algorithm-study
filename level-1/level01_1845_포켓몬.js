c28fc8405ed67cc66b8cfd56fe9f904bfd30e45d
Johan <johan@romantech.net>
2021-10-27 16:15:31 +0900

    :triangular_flag_on_post: Add "level01_42889_실패율"

--- level-1/level01_1845_포켓몬.js
+++ level-1/level01_1845_포켓몬.js
@@ -1,12 +1,10 @@
+/* eslint-disable no-unused-vars */
 function solution(nums) {
   // maxNum 구하고
   // 중복값 제거
   const maxNum = nums.length / 2;
-  const deDuplicate = [...new Set(nums)];
-  if (deDuplicate.length < maxNum) {
-    return deDuplicate.length;
-  }
-  return maxNum;
+  const deDuplicateLen = [...new Set(nums)].length;
+  return deDuplicateLen < maxNum ? deDuplicateLen : maxNum;
 }
 
 const c1 = [3, 1, 2, 3]; // 2
---
+++ level-1/level01_42889_실패율.js
@@ -0,0 +1,29 @@
+// N: 스테이지 개수
+// stages: 1~200000 이하 자연수, 각 요소는 사용자가 현재 도전중인 스테이지 번호
+// stages 요소가 N + 1과 같다면 마지막 스테이지까지 클리어한 사용자
+// 실패율이 높은 스테이지부터 내림차순으로 스테이지의 번호가 담겨있는 배열 리턴
+// 실패율이 같으면 작은 번호의 스테이지가 먼저 오도록
+// 스테이지에 도달한 유저가 없다면 실패율은 0으로 설정
+// 실패율 = 스테이지에 도달했으나 클리어하지 못한 플레이어 수 / 스테이지에 도달한 플레이어 수
+
+function solution(N, stages) {
+  // 해당 스테이지를 도전중인 유저 수 -> 현재 N 숫자와 같거나 큰
+  const r = Array.from(Array(N).keys(), n => n + 1).reduce(
+    (result, stageNum) => {
+      const clearNum = stages.filter(n => n >= stageNum);
+      const unClearNum = clearNum.filter(n => stageNum + 1 > n);
+      const rate = unClearNum.length / clearNum.length;
+      return result.concat({ stageNum, rate });
+    },
+    [],
+  );
+
+  return r.sort((a, b) => b.rate - a.rate).map(e => e.stageNum);
+}
+
+const case1 = {
+  N: 5,
+  stages: [2, 1, 2, 6, 2, 4, 3, 3],
+};
+
+solution(case1.N, case1.stages); /* ? */
