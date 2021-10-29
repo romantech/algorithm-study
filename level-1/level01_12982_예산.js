475e445b7ab0dd7efbfe13618575d674cdb5ab8d
Johan <johan@romantech.net>
2021-10-29 14:31:50 +0900

    :memo: Update "level01_12982" reference code

--- level-1/level01_12982_예산.js
+++ level-1/level01_12982_예산.js
@@ -1,4 +1,4 @@
-function solution(d, budget) {
+function solution1(d, budget) {
   // 최대한 많은 부서를 지원해줘야 하므로 작은 순서대로 sort
   const sorted = d.sort((a, b) => a - b);
   let maxNum = 0;
@@ -15,6 +15,17 @@ function solution(d, budget) {
   return result;
 }
 
+// * 레퍼런스
+// 매번 모든 요소를 순회해야돼서 효율이 좋다고는 할 수 없음.
+function solution2(d, budget) {
+  d.sort((a, b) => a - b); // [1, 2, 3, 4, 5]
+  while (d.reduce((a, b) => a + b, 0) > budget) d.pop();
+  // 15 > 9 -> [1, 2, 3, 4]
+  // 10 > 9 -> [1, 2, 3]
+
+  return d.length;
+}
+
 const case1 = {
   d: [1, 3, 2, 5, 4],
   b: 9,
@@ -25,4 +36,4 @@ const case2 = {
   b: 10,
 };
 
-solution(case2.d, case2.b); /* ? */
+solution1(case2.d, case2.b); /* ? */
