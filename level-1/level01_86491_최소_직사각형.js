2437d19ef74d315b18918f528deeda3572ffa5d9
Johan <johan@romantech.net>
2021-10-30 15:36:09 +0900

    :memo: Update "level01_86491" reference&review

--- level-1/level01_86491_최소_직사각형.js
+++ level-1/level01_86491_최소_직사각형.js
@@ -1,7 +1,7 @@
 // 모든 명함을 수납할 수 있는 가장 작은 사이즈의 명함 케이스의 가로*세로 값 리턴
 // 가로, 세로 회전 가능
 
-function solution(sizes) {
+function solution1(sizes) {
   // 최소 단위 구하기
   const maxNum = sizes.reduce(
     (acc, [a, b]) => Math.max(acc, Math.max(a, b)),
@@ -12,7 +12,7 @@ function solution(sizes) {
   sizes.forEach(([s1, s2]) => {
     const minNum = Math.min(s1, s2);
     if (result[1] < minNum) {
-      result[1] = minNum;
+      result[1] = minNum; // result[1] = Math.max(result[1], minNum)
     }
   });
 
@@ -26,4 +26,24 @@ const case1 = [
   [80, 40],
 ]; // 4000 (80*50)
 
-solution(case1); /* ? */
+const case2 = [
+  [10, 7],
+  [12, 3],
+  [8, 15],
+  [14, 7],
+  [5, 15],
+]; // 120 (8*15)
+
+// * 레퍼런스
+function solution2(sizes) {
+  const [hor, ver] = sizes.reduce(
+    ([h, v], [a, b]) => [
+      Math.max(h, Math.max(a, b)), // 가장 큰 수
+      Math.max(v, Math.min(a, b)), // v는 다른 요소의 최소 크기이므로 a, b는 v와 같거나 커야함
+    ],
+    [0, 0],
+  );
+  return hor * ver;
+}
+
+solution2(case1); /* ? */
