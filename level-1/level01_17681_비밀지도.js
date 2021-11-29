016907095bfb8f6f18492814a5aecc08e2b7ce0c
Johan <johan@romantech.net>
2021-11-30 02:42:00 +0900

    :memo: Update "level01_17681" reference & review

--- level-1/level01_17681_비밀지도.js
+++ level-1/level01_17681_비밀지도.js
@@ -1,4 +1,5 @@
-function solution(n, arr1, arr2) {
+/* eslint-disable no-bitwise */
+function solution1(n, arr1, arr2) {
   // 2진수 변환
   const converter = arr => {
     return arr.reduce((acc, cur) => {
@@ -37,4 +38,33 @@ const c2 = {
   arr2: [27, 56, 19, 14, 14, 10],
 }; // ["######", "### #", "## ##", " #### ", " #####", "### # "]
 
-solution(c2.n, c2.arr1, c2.arr2); /* ? */
+solution1(c2.n, c2.arr1, c2.arr2); /* ? */
+
+// * 레퍼런스
+
+function solution2(n, arr1, arr2) {
+  /**
+   * v | arr2[i]는 비트연산자의 OR 논리연산자(bitwise; single vertical bar).
+   * 비교하는 이진수 숫자의 자리수가 하나라도 1이라면 1반환
+   * 46의 2진수는 101110, 27의 2진수는 11011
+   * 101110 (46)
+   * 011011 (27)
+   * ------
+   * 111111 (63)
+   * 따라서 47 | 27 = 63
+   */
+
+  return arr1.map((v, i) =>
+    // +a는 문자열을 숫자로 변경해줌. +'0' -> 0
+    // 정규식에서 | 교체구문은 OR 연산자와 동일. 현재 문자열이 1이나 0이면 replace 진행
+    addZero(n, (v | arr2[i]).toString(2)).replace(/1|0/g, a =>
+      +a ? '#' : ' ',
+    ),
+  );
+}
+
+const addZero = (n, s) => {
+  return '0'.repeat(n - s.length) + s;
+};
+
+solution2(c2.n, c2.arr1, c2.arr2); /* ? */
