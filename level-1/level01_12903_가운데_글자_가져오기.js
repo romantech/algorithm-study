ca5a3dc9b7274fefa75be692f1de239c93a8eb30
Johan <johan@romantech.net>
2021-12-05 17:13:18 +0900

    :memo: Update "level01_12903" reference

--- level-1/level01_12903_가운데_글자_가져오기.js
+++ level-1/level01_12903_가운데_글자_가져오기.js
@@ -1,13 +1,10 @@
 // 단어 s의 가운데 글자 반환
 // 단어 길이 짝수면 가운데 두 글자 반환
-
-function solution(s) {
+function solution1(s) {
   const len = s.length;
-  const halfLen = len / 2;
+  const halfLen = s.length / 2;
 
-  if (len % 2 === 0) {
-    return s[halfLen - 1] + s[halfLen];
-  }
+  if (len % 2 === 0) return s[halfLen - 1] + s[halfLen];
   return s[Math.floor(halfLen)];
 }
 
@@ -15,4 +12,13 @@ const c1 = 'abcde'; // c
 const c2 = 'qwer'; // we
 
 // abcdefgh
+solution1(c1); /* ? */
+
+// 레퍼런스 참고해서 개선한 코드
+function solution(s) {
+  const start = Math.ceil(s.length / 2) - 1;
+  const end = Math.floor(s.length / 2) + 1;
+  return s.slice(start, end);
+}
+
 solution(c1); /* ? */
