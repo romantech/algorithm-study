92c7e6f61ab18a655abd47e7948f05eff5d3e3c3
Johan <johan@romantech.net>
2021-12-12 18:46:54 +0900

    :memo: Update "level01_12915" reference & review

--- level-1/level01_12915_문자열_내_마음대로_정렬하기.js
+++ level-1/level01_12915_문자열_내_마음대로_정렬하기.js
@@ -22,3 +22,16 @@ const c2 = {
 }; // 	["abcd", "abce", "cdx"]
 
 solution(c2.strings, c2.n); /* ? */
+
+// 레퍼런스
+function solution2(strings, n) {
+  // str1.localeCompare(str2) : str1이 str2보다 큰지/작은지/같은지 확인(사전식 비교)
+  // 'a'.localeCompare('b') // -1 (a(97)가 b(98)보다 작으므로 -1)
+  // 'b'.localeCompare('a') // 1 (b(98)가 a(97)보다 크므로 1)
+  // 'abcd'.localeCompare('abce') // -1 -> 'abcd'(97+98+99+100)가 abce(97+98+99+101)보다 작으므로
+  // 마찬가지로 자바스크립트에서 문자열은 사전식 비교(유니코드 순서) 'abcd' < 'abce' // true
+
+  return strings.sort((s1, s2) =>
+    s1[n] === s2[n] ? s1.localeCompare(s2) : s1[n].localeCompare(s2[n]),
+  );
+}
