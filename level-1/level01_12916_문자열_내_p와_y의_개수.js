176e2648d907a12a654a42b9eff82cd556b384a6
Johan <johan@romantech.net>
2021-12-16 01:00:54 +0900

    :memo: Update "level01_12916" reference & review

--- level-1/level01_12916_문자열_내_p와_y의_개수.js
+++ level-1/level01_12916_문자열_내_p와_y의_개수.js
@@ -20,3 +20,22 @@ const c1 = 'pPoooyY'; // true
 const c2 = 'Pyy'; // false
 
 solution(c1); /* ? */
+
+// 레퍼런스 1
+function numPY1(s) {
+  return (
+    s.toUpperCase().split('P').length === s.toUpperCase().split('Y').length
+    // 'PPOOOYY'를 대문자로 바꾼후 각각 split('P'), split('P')하면
+    // ['', '', 'OOOYY'], ['PPOOO', '', '']
+    // P혹은 Y는 빈문자열로 나오고 나머진 모두 한개 문자열('YY'등)로 나오므로
+    // split 후 length가 같다면 P와 Y의 개수도 같음
+  );
+}
+
+// 레퍼런스 2
+function numPY2(s) {
+  return s.match(/p/gi).length === s.match(/y/gi).length;
+  // i 플래그는 대소문자 구분 없음
+  // 'pPoooyY'.match(/p/gi) -> ['p', 'P']
+  // 'pPoooyY'.match(/y/gi) -> ['y', 'Y']
+}
