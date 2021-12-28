1a615bdfd7bdad1c6bd685ee5d92788904b09142
Johan <johan@romantech.net>
2021-12-29 01:18:01 +0900

    :memo: Update "level01_12930" reference & review

--- level-1/level01_12930_이상한_문자_만들기.js
+++ level-1/level01_12930_이상한_문자_만들기.js
@@ -18,3 +18,21 @@ function solution(s) {
 
 const c1 = 'try hello world'; // "TrY HeLlO WoRlD"
 solution(c1); /* ? */
+
+// 레퍼런스
+function toWeirdCase(s) {
+  return s.toUpperCase().replace(/(\w)(\w)/g, function (a) {
+    return a[0].toUpperCase() + a[1].toLowerCase();
+  });
+
+  // 'TRY HELLO WORLD'.match(/(\w\)(w/)g) -> 'TR', 'HE', 'LL', 'WO', 'RL'
+  // \w는 단어문자(대소문자a~z, 0~9, _)만 매칭됨
+  // \w\w 두번이므로 단어문자가 2번 연속으로 와야 매칭됨
+  // 'TR' -> 단어 문자 두 번 연속이므로 매칭
+  // 'Y ' -> 단어 문자와 공백이 있으므로 매칭되지 않음
+  // 따라서 'TR', 'HE', 'LL', 'WO', 'RL' 이 매칭됨
+  // 단어 2개씩 매칭하므로, 매칭되지 않은 문자열은 2번, 4번... 인덱스이므로 항상 대문자
+  // 이 때문에 toUpperCase로 대문자로 만든 후 2개 단어문자씩 끊어서 매칭하도록 한 것
+}
+
+toWeirdCase(c1); /* ? */
