31c0aeeb7c4f62b2d6ef72c0526ed703c95e0b4a
Johan <johan@romantech.net>
2022-01-02 18:12:10 +0900

    :triangular_flag_on_post: Add "level01_12935"

---
+++ level-1/level01_12935_제일_작은_수_제거하기.js
@@ -0,0 +1,32 @@
+// arr의 가장 작은 수를 제거한 배열을 리턴하는 함수
+
+function solution(arr) {
+  if (arr.length <= 1) return [-1];
+  const minNum = Math.min(...arr);
+  return arr.filter(el => el !== minNum);
+}
+
+const c1 = {
+  c: [4, 3, 2, 1],
+  a: [4, 3, 2],
+};
+
+const c2 = {
+  c: [10],
+  a: [-1],
+};
+
+const c3 = {
+  c: [2],
+  a: [-1],
+};
+
+[c1, c2, c3].forEach(({ c, a }, idx) => {
+  const res = solution(c);
+
+  if (JSON.stringify(res) !== JSON.stringify(a)) {
+    console.log(`${idx + 1}번 케이스 실패`);
+  } else {
+    console.log(`${idx + 1}번 케이스 통과`);
+  }
+});
