fccf81391804e8a787e7625f77ecf75d95d9abf4
Johan <johan@romantech.net>
2022-03-30 23:43:19 +0900

    📝 Update: "level01_12948" reference

--- level-1/level01_12948_핸드폰_번호_가리기.js
+++ level-1/level01_12948_핸드폰_번호_가리기.js
@@ -8,6 +8,14 @@ function solution(number) {
   }, '');
 }
 
+// reference
+function solution2(s) {
+  return '*'.repeat(s.length - 4) + s.slice(-4);
+  // '*'.repeat(s.length -4) -> 파라미터 뒷 4자리를 제외한 length 만큼 '*' 문자열 생성
+  // s.slice(-4) -> 뒤에서 4자리만큼 자르기
+  // ex) '123456789'.slice(-4) -> '6789'
+}
+
 const testCase = [
   {
     input: '01033334444',
@@ -22,7 +30,7 @@ const testCase = [
 testCase.forEach(({ input, expectedResult }) =>
   console.log('핸드폰 번호 가리기', {
     input,
-    output: solution(input),
-    passed: solution(input) === expectedResult,
+    output: solution2(input),
+    passed: solution2(input) === expectedResult,
   }),
 );
