cdc952fa3e4b9fba1582fba77fe5fab036791d40
Johan <johan@romantech.net>
2022-06-06 03:29:41 +0900

    :bulb: 병합 정렬 주석 수정

--- src/sort-algorithm/mergeSort.js
+++ src/sort-algorithm/mergeSort.js
@@ -3,15 +3,15 @@
 import { Compare, defaultCompare, makeRandomArr } from '../utils.js';
 
 const merge = (left, right, compare) => {
-  let i = 0;
-  let j = 0;
+  let i = 0; // left 리스트 인덱스
+  let j = 0; // right 리스트 인덱스
   const result = [];
-  // left 혹은 right 의 모든 요소가 결과 배열에 추가될 때까지 반복
+  // left 혹은 right 의 모든 요소가 result 배열에 추가될 때까지 반복
   while (i < left.length && j < right.length) {
     const isLeft = compare(left[i], right[j]) === Compare.LESS_THAN;
     result.push(isLeft ? left[i++] : right[j++]);
   }
-  // 나머지 리스트의 남은 요소를 결과 배열에 추가
+  // 남은 요소를 result 배열에 추가
   return result.concat(i < left.length ? left.slice(i) : right.slice(j));
 };
 
@@ -19,10 +19,10 @@ const mergeSort = (arr, compare = defaultCompare) => {
   // 배열 길이가 1이 될때까지 배열 분할
   if (arr.length > 1) {
     const { length } = arr;
-    const middle = Math.floor(length / 2); // 분할 | [5, 4, 3, 2, 1] -> middle: 2, ...
-    const left = mergeSort(arr.slice(0, middle)); // 정복 | [5, 4], ...
-    const right = mergeSort(arr.slice(middle, length)); // 정복 | [3, 2, 1], ...
-    arr = merge(left, right, compare); // 결합
+    const middle = Math.floor(length / 2); // [분할] | [5, 4, 3, 2, 1] -> middle: 2, ...
+    const left = mergeSort(arr.slice(0, middle)); // [정복] | [5, 4], ...
+    const right = mergeSort(arr.slice(middle, length)); // [정복] | [3, 2, 1], ...
+    arr = merge(left, right, compare); // [결합]
   }
   return arr;
 };
