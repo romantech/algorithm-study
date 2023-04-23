56c96f65f4015ee35cf313bde4aacb917418e9ae
ColorFilter <johan@romantech.net>
2023-04-23 22:08:58 +0900

    :memo: level01_133499 레퍼런스/리뷰 추가

--- src/programmers/level-01/level01_133499_옹알이(2).js
+++ src/programmers/level-01/level01_133499_옹알이(2).js
@@ -1,3 +1,5 @@
+/* eslint-disable no-param-reassign */
+
 // "aya", "ye", "woo", "ma"를 조합해서 발음할 수 있는 개수 반환
 // 중복 발음은 불가
 // ex) ["ayaye", "uuu", "yeye", "yemawoo", "ayaayaa"]
@@ -26,3 +28,22 @@ const cases = [
 ];
 
 solution(cases[0].input);
+
+// 레퍼런스 (여러 레퍼런스 보면서 입맛에 맞게 수정함)
+function solution2(babbling) {
+  let answer = 0;
+
+  babbling.forEach(word => {
+    // aya|ye|woo|ma 중 하나를 캡처하여 yeye 이렇게 한 번더 중복되는걸 찾고 '-'로 변경
+    // e.g. yeye -> ye를 \1로 캡쳐 -> (ye)\1 -> yeye -> 중복되므로 '-'로 변경
+    // 참고로 + 수량자는 1번 이상 반복할 때 일치한다고 판단 e.g. (ye)\1+는 yeye+가 되므로 ye가 2번 이상 반복할 때 일치한다고 판단
+    word = word.replaceAll(/(aya|ye|woo|ma)\1/g, '-');
+
+    word = word.replaceAll(/(aya|ye|woo|ma)/g, '+');
+    if (word.replaceAll('+', '').length === 0) answer++;
+  });
+
+  return answer;
+}
+
+solution2(cases[0].input);
