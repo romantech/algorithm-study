17a154a1001a88020136092a482449850972a933
ColorFilter <johan@romantech.net>
2023-04-28 17:58:17 +0900

    :triangular_flag_on_post: level01_134240 추가

---
+++ src/programmers/level-01/level01_134240_푸드 파이트.js
@@ -0,0 +1,23 @@
+// 음색 배치 반환
+// 가운데는 0
+// food 배열은 칼로리가 낮은 순서
+// 1은 버리고, 2~3는 1, 4부턴 나눈 몫
+function solution(food) {
+  let left = '';
+
+  for (let i = 0; i < food.length; i += 1) {
+    // 1 / 2 = 0.5 -> parseInt(0.5) -> 0
+    const count = parseInt(food[i] / 2, 10);
+    const counted = String(i).repeat(count);
+    left += counted;
+  }
+
+  return left + 0 + [...left].reverse().join('');
+}
+
+const cases = [
+  { input: [1, 3, 4, 6], output: '1223330333221' },
+  { input: [1, 7, 1, 2], output: '111303111' },
+];
+
+solution(cases[1].input);
