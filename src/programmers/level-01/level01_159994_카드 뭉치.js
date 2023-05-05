4e804c1f16ed4ffbbc91bb86ea7d899e02308770
ColorFilter <johan@romantech.net>
2023-05-05 13:08:29 +0900

    :memo: level01_159994 레퍼런스 추가

--- src/programmers/level-01/level01_159994_카드 뭉치.js
+++ src/programmers/level-01/level01_159994_카드 뭉치.js
@@ -37,3 +37,17 @@ function solution(cards1, cards2, goal) {
 }
 
 solution(...cases[0].input);
+
+// 레퍼런스
+function solution2(cards1, cards2, goal) {
+  let j = 0;
+  let k = 0;
+  for (let i = 0; i < goal.length; i++) {
+    if (goal[i] === cards1[j]) j++; // i(1) > drink(2) > water(3)
+    else if (goal[i] === cards2[k]) k++; // want(1) > to(2)
+    else return 'No';
+  }
+  return 'Yes';
+}
+
+solution2(...cases[0].input);
