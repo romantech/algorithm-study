2f1f714492958f5114802406f888e29a64f0a54a
ColorFilter <johan@romantech.net>
2023-09-28 01:58:12 +0900

    :recycle: level02_12913 반복문 코드 추가 및 시간 복잡도 주석 추가

--- src/programmers/level-02/level02_12913_땅따먹기.js
+++ src/programmers/level-02/level02_12913_땅따먹기.js
@@ -1,4 +1,5 @@
 // 각 행의 가장 높은 숫자의 합 반환
+// land 입력값 : 행 개수 <= 100,000, 열 개수 4로 이루어진 2차원 배열
 // 조건: 같은 열을 연속해서 선택할 수 없음
 // 두번째 행부터 각 열을 선택했을 때 얻을 수 있는 최대 점수 저장(현재 열을 제외한 이전 행의 최대값)
 // 이 과정을 반복하면 마지막 행에서 최대 점수를 얻을 수 있음
@@ -7,23 +8,34 @@ function solution(land) {
   const row = land.length;
   const column = land[0].length;
 
-  // DP 배열 초기화
+  // DP 배열 초기화 O(4n) -> 최고 차수의 항만 표시하므로 O(n)
   const dp = Array.from({ length: row }, () => Array(column).fill(0));
   [dp[0]] = land;
 
   // 하드 코딩 버전
+  // for (let i = 1; i < row; i++) {
+  //   // 현재 행의 첫번째 위치를 선택했을 때 얻을 수 있는 최대 점수 저장
+  //   dp[i][0] = Math.max(dp[i - 1][1], dp[i - 1][2], dp[i - 1][3]) + land[i][0];
+  //   // 현재 행의 두번째 위치를 선택했을 때 얻을 수 있는 최대 점수 저장
+  //   dp[i][1] = Math.max(dp[i - 1][0], dp[i - 1][2], dp[i - 1][3]) + land[i][1];
+  //   // 현재 행의 세번째 위치를 선택했을 때 얻을 수 있는 최대 점수 저장
+  //   dp[i][2] = Math.max(dp[i - 1][0], dp[i - 1][1], dp[i - 1][3]) + land[i][2];
+  //   // 현재 행의 네번째 위치를 선택했을 때 얻을 수 있는 최대 점수 저장
+  //   dp[i][3] = Math.max(dp[i - 1][0], dp[i - 1][1], dp[i - 1][2]) + land[i][3];
+  // }
+
+  // 열의 개수는 항상 4이므로 이중 for문의 시간 복잡도는 O(4n) -> O(n)
   for (let i = 1; i < row; i++) {
-    // 현재 행의 첫번째 위치를 선택했을 때 얻을 수 있는 최대 점수 저장
-    dp[i][0] = Math.max(dp[i - 1][1], dp[i - 1][2], dp[i - 1][3]) + land[i][0];
-    // 현재 행의 두번째 위치를 선택했을 때 얻을 수 있는 최대 점수 저장
-    dp[i][1] = Math.max(dp[i - 1][0], dp[i - 1][2], dp[i - 1][3]) + land[i][1];
-    // 현재 행의 세번째 위치를 선택했을 때 얻을 수 있는 최대 점수 저장
-    dp[i][2] = Math.max(dp[i - 1][0], dp[i - 1][1], dp[i - 1][3]) + land[i][2];
-    // 현재 행의 네번째 위치를 선택했을 때 얻을 수 있는 최대 점수 저장
-    dp[i][3] = Math.max(dp[i - 1][0], dp[i - 1][1], dp[i - 1][2]) + land[i][3];
+    for (let j = 0; j < column; j++) {
+      const prevRow = dp[i - 1].filter((_, index) => index !== j); // O(4) -> 상수항 무시하므로 O(1)
+      const prevRowMax = Math.max(...prevRow);
+      dp[i][j] = prevRowMax + land[i][j];
+    }
   }
 
   return Math.max(...dp.at(-1));
+  // solution 함수의 시간 복잡도 : DP 배열 초기화 O(n) + 이중 for문 O(n)
+  // 가장 큰 차수만 표시하므로 전체 함수의 시간 복잡도 : O(n)
 }
 
 /*
@@ -65,4 +77,4 @@ const cases = [
   },
 ];
 
-console.log(solution(cases[1].input));
+console.log(solution(cases[0].input));
