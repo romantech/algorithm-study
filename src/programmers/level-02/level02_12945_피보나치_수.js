034384dbd49564a47e2bcb59b26b2ef66d0cb767
ColorFilter <johan@romantech.net>
2023-10-14 17:59:49 +0900

    :memo: level02_12945 주석 추가

--- src/programmers/level-02/level02_12945_피보나치_수.js
+++ src/programmers/level-02/level02_12945_피보나치_수.js
@@ -1,9 +1,13 @@
+// n번째 피보나치수를 1234567로 나눈 나머지 값 반환 (0~1 피보나치수는 그대로 반환)
+
 function solution(n) {
   const memo = [0, 1];
   const MOD = 1234567;
+
   for (let i = 2; i <= n; i += 1) {
     memo[i] = (memo[i - 1] + memo[i - 2]) % MOD;
   }
+
   return memo[n];
 }
 
