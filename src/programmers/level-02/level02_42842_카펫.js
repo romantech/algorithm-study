58ac88bb7ee3ee9264b32e2ffbabc7b81a4c6300
ColorFilter <johan@romantech.net>
2023-12-03 23:19:24 +0900

    :memo: level02_42842 레퍼런스 코드 및 설명 추가

--- src/programmers/level-02/level02_42842_카펫.js
+++ src/programmers/level-02/level02_42842_카펫.js
@@ -1,8 +1,10 @@
+/* eslint-disable consistent-return */
+
 import { generateTestPair } from '../../utils.js';
 
 /**
  * [요구사항]
- * 갈색 격자 수 brown, 노란색 격자 수 yellow가 주어질 때 카펫의 가로, 세로를 배열에 담아서 리턴
+ * 갈색 타일 수 brown, 노란색 타일 수 yellow가 주어질 때 카펫의 가로, 세로를 배열에 담아서 리턴
  * [매개변수]
  * brown: 8 이상 5000이하 자연수
  * yellow: 1 이상 2000000이하 자연수
@@ -37,6 +39,42 @@ function solution(brown, yellow) {
   return [yellowWidth + 2, yellowHeight + 2];
 }
 
+function reference(brown, yellow) {
+  const total = brown + yellow; // 전체 타일의 개수 = 가로 * 세로
+
+  /**
+   * 문제에서 brown의 최소 값은 8이므로 최소 높이는 3
+   * b b b
+   * b Y b
+   * b b b
+   * 가로, 세로 길이는 전체 타일 개수의 약수에서 찾을 수 있음
+   * 예를들어 전체 타일의 수가 12라면 약수 1, 2, 3, 4, 6, 12 중에서 4(가로)와 3(세로)을 곱하면 됨
+   *
+   * Math.sqrt(total) 조건을 주는 이유:
+   * 문제에서 세로는 가로보다 클 수 없기 때문에, 세로는 항상 가로의 최대값인, 전체 타일 수의 제곱근 이하의 수가 됨
+   * 예를들어 가로 4, 세로 3, 전체 타일 수 12에서 세로 길이 3은 전체 타일의 제곱근 3.46... 보다 같거나 작음
+   * 또한, 제곱근 이후부터 이미 계산했던 약수를 중복해서 계산하므로 제곱근 이후부턴 계산할 필요 없음
+   */
+
+  for (let height = 3; height <= Math.sqrt(total); height++) {
+    // 높이가 전체 타일 개수의 약수라면
+    if (total % height === 0) {
+      const width = total / height; // total = width * height 이므로 width = total / height
+      /**
+       * 노란색 타일 너비 = 갈색 타일 너비 - 2, 노란색 타일 높이 = 갈색 타일 높이 - 2
+       * 예를들어 갈색 타일의 가로/세로가 각각 4, 3이면, 노란색 타일의 가로는 2(4-2), 1(3-2) 이 됨
+       * b b b b
+       * b Y Y b
+       * b b b b
+       *
+       */
+      if ((width - 2) * (height - 2) === yellow) {
+        return [width, height];
+      }
+    }
+  }
+}
+
 const cases = [
   generateTestPair([10, 2], [4, 3]), // [brown, yellow], [x, y]
   generateTestPair([8, 1], [3, 3]),
@@ -44,6 +82,6 @@ const cases = [
 ];
 
 cases.forEach(({ input, output }) => {
-  const result = solution(...input);
-  console.log(result.every((el, i) => el === output[i]));
+  console.log(solution(...input).every((el, i) => el === output[i]));
+  console.log(reference(...input).every((el, i) => el === output[i]));
 });
