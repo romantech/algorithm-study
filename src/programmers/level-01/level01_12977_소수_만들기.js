517e5fdbbf786adc201157ccd6e8c360487df83a
ColorFilter <johan@romantech.net>
2023-12-31 03:31:48 +0900

    :recycle: isPrime 함수 import 해서 사용하도록 변경

--- src/programmers/level-01/level01_12977_소수_만들기.js
+++ src/programmers/level-01/level01_12977_소수_만들기.js
@@ -1,22 +1,11 @@
+import { isPrime } from '../../utils.js';
+
 // 주어진 숫자 중 3개의 수를 더했을 때 소수가 되는 경우의 수 구하기
 // nums에 들어있는 숫자의 개수 : 3~50
 // nums 각 원소 1~1000이하 자연수, 중복된 숫자 없음
 
 function solution(nums) {
   let answer = 0;
-
-  function isPrime(sum) {
-    // 소수는 나누어 떨어지는 수가 1과 자신밖에 없는 수
-    // 1은 소수가 아님
-    // 2는 소수중에 유일한 짝수
-    if (sum <= 1) return false;
-    if (sum === 2) return true;
-
-    for (let i = 2; i <= Math.sqrt(sum); i++) {
-      if (sum % i === 0) return false;
-    }
-    return true;
-  }
 
   const len = nums.length;
   for (let i = 0; i < len; i++) {
