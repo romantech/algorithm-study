d78611581ea155bf599d1362e5c1a145403ca753
ColorFilter <johan@romantech.net>
2024-01-01 01:52:36 +0900

    :triangular_flag_on_post: 조합을 생성하는 getCombinations 함수 추가

--- src/math.js
+++ src/math.js
@@ -12,6 +12,48 @@ export const isPrime = num => {
 };
 
 /**
+ * Generate all possible combinations of a given size from an array.
+ *
+ * @param {Array} arr - the input array from which combinations will be generated
+ * @param {number} combSize - the size of each combination
+ * @param {number} start - the starting index for generating combinations (default is 0)
+ * @param {Array} curComb - the current combination being built (default is an empty array)
+ * @return {Array} - an array containing all possible combinations of the given size
+ */
+export const getCombinations = (arr, combSize, start = 0, curComb = []) => {
+  if (curComb.length === combSize) return [curComb]; // 목표 크기의 조합을 완성했을 때
+  if (start === arr.length) return []; // 더이상 조합을 생성할 수 없을 때
+
+  return arr.slice(start).reduce((allCombs, item, i) => {
+    const nextComb = curComb.concat(item);
+    const nextStart = start + i + 1;
+    return allCombs.concat(getCombinations(arr, combSize, nextStart, nextComb));
+  }, []);
+};
+
+/*
+getCombinations([1, 2, 3], 2, 0, [])
+
+sliced = [1, 2, 3], i0, nextComb = [1], nextStart = (0 + 0 + 1) = 1
+  getCombinations([1, 2, 3], 2, 1, [1])
+  sliced = [2, 3], i0, nextComb = [1, 2], nextStart = (1 + 0 + 1) = 2
+    getCombinations([1, 2, 3], 2, 2, [1, 2]) -> return [[1, 2]]
+  sliced = [2, 3], i1, nextComb = [1, 3], nextStart = (1 + 1 + 1) = 3
+    getCombinations([1, 2, 3], 2, 3, [1, 3]) -> return [[1, 3]]
+  return [[1, 2], [1, 3]]
+
+sliced = [1, 2, 3], i1, nextComb = [2], nextStart = (0 + 1 + 1) = 2
+  getCombinations([1, 2, 3], 2, 2, [2])
+  sliced = [3], i0, nextComb = [2, 3], nextStart = (2 + 0 + 1) = 3
+    getCombinations([1, 2, 3], 2, 3, [2, 3]) -> return [[2, 3]]
+  return [[2, 3]]
+
+sliced = [1, 2, 3], i2, nextComb = [3], nextStart = (0 + 2 + 1) = 3
+  getCombinations([1, 2, 3], 2, 3, [3]) -> return []
+  return []
+*/
+
+/**
  * Generates permutations of an array.
  *
  * @param {Array} arr - The array to generate permutations from.
@@ -31,7 +73,7 @@ export const getPermutations = (arr, permSize = arr.length, curPerm = []) => {
 };
 
 /*
-getPermutations([1, 2, 3], [])
+getPermutations([1, 2, 3], []) 중간 permSize 생략
 
 r = [2, 3], p = [1]
   getPermutations([2, 3], [1])
