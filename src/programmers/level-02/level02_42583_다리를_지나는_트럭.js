c91753fb84eca0e3c7bbea316123ea71aa79d8ad
ColorFilter <johan@romantech.net>
2024-01-31 13:45:00 +0900

    :triangular_flag_on_post: level02_42583 구현

--- src/programmers/level-02/level02_42583_다리를_지나는_트럭.js
+++ src/programmers/level-02/level02_42583_다리를_지나는_트럭.js
@@ -10,6 +10,12 @@ import { generateTestPair } from '../../utils.js';
  * 다리는 weight 이하까지 견딜 수 있고, 다리에 완전히 오르지 않은 트럭의 무게는 무시
  * 트럭 순서는 바꿀 수 없음. 각 트럭들은 하나의 단위시간에 하나의 단위길이만큼만 이동 가능
  *
+ * [파라미터]
+ * bridge_length : 1 이상 10,000 이하
+ * weight : 1 이상 10,000 이하
+ * truck_weight : 1 이상 10,000 이하, 각 트럭의 무게는 1 이상 weight 이하
+ *
+ * [예시]
  * 예를들어 트럭 2대가 올라갈 수 있고(bridge_length = 2),
  * 무게를 10kg(weight = 10)까지 견디는 다리가 있음
  * 무게가 [7, 4, 5 ,6]kg인 트럭이 순서대로 최단 시간 안에 다리를 건너려면...
@@ -26,12 +32,44 @@ import { generateTestPair } from '../../utils.js';
  */
 
 function solution(bridge_length, weight, truck_weights) {
-  const answer = 0;
-  return answer;
+  let elapsedTime = 0;
+  let totalWeightOnBridge = 0;
+  const trucksOnBridge = []; // [{ weight, distanceLeft }]
+
+  while (truck_weights.length > 0 || trucksOnBridge.length > 0) {
+    elapsedTime++;
+
+    // 다리 위에 있는 트록 앞으로 이동
+    trucksOnBridge.forEach(truck => truck.distanceLeft--);
+
+    // 가장 앞에 있는 트럭이 다리를 건넜는지 확인
+    if (trucksOnBridge[0]?.distanceLeft === 0) {
+      totalWeightOnBridge -= trucksOnBridge.shift().weight;
+    }
+
+    // 가능시 새로운 트럭을 다리 위로 이동
+    const nextTruckWeight = truck_weights[0];
+    if (
+      nextTruckWeight &&
+      totalWeightOnBridge + nextTruckWeight <= weight &&
+      trucksOnBridge.length < bridge_length
+    ) {
+      trucksOnBridge.push({
+        weight: nextTruckWeight,
+        distanceLeft: bridge_length,
+      });
+      totalWeightOnBridge += nextTruckWeight;
+      truck_weights.shift();
+    }
+  }
+
+  return elapsedTime;
 }
 
 const cases = [
-  generateTestPair([2, 10, [7, 4, 5, 6], 8]), // bridge_length, weight, truck_weights, return
+  generateTestPair([2, 10, [7, 4, 5, 6]], 8), // bridge_length, weight, truck_weights, return
   generateTestPair([100, 100, [10]], 101),
   generateTestPair([100, 100, [10, 10, 10, 10, 10, 10, 10, 10, 10, 10]], 110),
 ];
+
+console.log(solution(...cases[2].input));
