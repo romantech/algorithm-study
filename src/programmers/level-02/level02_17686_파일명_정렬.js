c561980228d22f9bc4ae986cdb45e2b625141b96
ColorFilter <johan@romantech.net>
2024-03-28 18:39:53 +0900

    level02-17686 구현

--- src/programmers/level-02/level02_17686_파일명_정렬.js
+++ src/programmers/level-02/level02_17686_파일명_정렬.js
@@ -5,6 +5,7 @@ import { generateTestPair } from '../../utils.js';
  * 현재 사전식으로 내림차순 정렬된 상태 e.g. "12", "10", "2", "1" -> "1", "10", "12", "2"
  * 사전 순이 아닌 실제 숫자 크기가 반영되도록 하고 싶어함 "1" -> "2" -> "10" -> "12"
  * 파일명은 영문자로 시작하며, 100 글자 이내, 영문 대소문자, 숫자, 공백, 마침표, 빼기 부호로 이루어져 있음
+ *
  * 파일명은 HEAD, NUMBER, TAIL 3 부분으로 구성
  * HEAD: 숫자가 아닌 문자, 최소 1글자
  * NUMBER: 1~5 사이의 연속된 숫자로 이루어짐 e.g. 00000, 0101
@@ -26,8 +27,30 @@ import { generateTestPair } from '../../utils.js';
  */
 
 function solution(files) {
-  const answer = [];
-  return answer;
+  const splitPart = file => {
+    // "^" 시작, "+" 1번 이상 일치, "*" 0번 이상 일치, "\D" 숫자 아님, "\d" 숫자, "." 임의 문자
+    const match = file.match(/^(\D+)(\d{1,5})(.*)$/);
+    if (!match) return { head: '', number: '', tail: '' };
+
+    // ["전체 매칭된 문자열", "첫번째 캡처 그룹", "두번째 캡처 그룹", "세번째 캡처 그룹"]
+    // 문자열을 숫자로 변환할 때 앞쪽에 있는 0은 모두 제거됨
+    const [, head, number, tail] = match;
+    return [head.toLowerCase(), parseInt(number, 10), tail];
+  };
+
+  const compareFn = (a, b) => {
+    const [aHead, aNumber] = splitPart(a);
+    const [bHead, bNumber] = splitPart(b);
+
+    if (aHead < bHead) return -1; // a가 더 작을 때, a를 b 앞으로 보냄
+    if (aHead > bHead) return 1; // a가 더 클 때, a를 b 뒤로 보냄
+    if (aNumber < bNumber) return -1;
+    if (aNumber > bNumber) return 1;
+
+    return 0;
+  };
+
+  return files.toSorted(compareFn);
 }
 
 const cases = [
