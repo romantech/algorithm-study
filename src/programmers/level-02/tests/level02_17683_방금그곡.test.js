74cf0184c3af46ae444c58101496831a1f4000af
ColorFilter <johan@romantech.net>
2024-03-31 17:57:12 +0900

    파일명 변경

--- src/programmers/level-02/tests/level02_17683_방금그곡.test.js
+++ src/programmers/level-02/tests/level02_17683_방금그곡.test.js
@@ -1,4 +1,4 @@
-import { cases, solution } from '../level02_17683_방금_그곡';
+import { cases, solution } from '../level02_17683_방금그곡';
 
 describe('프로그래머스 - 레벨 02 - 방금 그곡', () => {
   cases.forEach(({ input, output }) => {
