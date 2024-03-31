c4b8fe1329b351f48f09a67539d22ae73c55821d
ColorFilter <johan@romantech.net>
2024-03-31 17:57:35 +0900

    :triangular_flag_on_post: level02-17683 방금그곡 (#7)

---
+++ src/programmers/level-02/tests/level02_17683_방금그곡.test.js
@@ -0,0 +1,9 @@
+import { cases, solution } from '../level02_17683_방금그곡';
+
+describe('프로그래머스 - 레벨 02 - 방금 그곡', () => {
+  cases.forEach(({ input, output }) => {
+    test(`입력값: ${input} => 예상값: ${output}`, () => {
+      expect(solution(...input)).toEqual(output);
+    });
+  });
+});
