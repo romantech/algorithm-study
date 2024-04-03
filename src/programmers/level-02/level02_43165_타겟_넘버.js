f7705d5b2963a7f5df6e3f25934962cb6931ef7a
ColorFilter <johan@romantech.net>
2024-04-03 21:37:02 +0900

    코드 구현

--- src/programmers/level-02/level02_43165_타겟_넘버.js
+++ src/programmers/level-02/level02_43165_타겟_넘버.js
@@ -1,3 +1,5 @@
+import { generateTestPair } from '../../utils.js';
+
 /**
  * [요구사항]
  * n 개의 양의 정수들을 순서를 바꾸지 않고 더하거나 빼서 타겟 넘버를 만들 수 있는 방법의 수 반환
@@ -8,20 +10,32 @@
  *
  * [예시]
  * numbers: [1, 1, 1, 1, 1] | target: 3
- * 1. -1 + 1 + 1 + 1 + 1 = 3
- * 2. +1 - 1 + 1 + 1 + 1 = 3
- * 3. +1 + 1 - 1 + 1 + 1 = 3
- * 4. +1 + 1 + 1 - 1 + 1 = 3
- * 5. +1 + 1 + 1 + 1 - 1 = 3
+ * 1. -1 +1 +1 +1 +1 = 3
+ * 2. +1 -1 +1 +1 +1 = 3
+ * 3. +1 +1 -1 +1 +1 = 3
+ * 4. +1 +1 +1 -1 +1 = 3
+ * 5. +1 +1 +1 +1 -1 = 3
  * return 5
  *
  * numbers: [4, 1, 2, 1] | target: 4
- * 1. +4 + 1 - 2 + 1 = 4
- * 2. +4 - 1 + 2 - 1 = 4
+ * 1. +4 +1 -2 +1 = 4
+ * 2. +4 -1 +2 -1 = 4
  * return 2
  */
 
 function solution(numbers, target) {
-  const answer = 0;
-  return answer;
+  const dfs = (index, sum) => {
+    if (index === numbers.length) return sum === target ? 1 : 0;
+
+    const add = dfs(index + 1, sum + numbers[index]);
+    const sub = dfs(index + 1, sum - numbers[index]);
+
+    return add + sub;
+  };
+
+  return dfs(0, 0);
 }
+
+const cases = [generateTestPair([[1, 1, 1, 1, 1], 3], 5), generateTestPair([[4, 1, 2, 1], 4], 2)];
+
+console.log(solution(...cases[0].input));
