53335d95436fc5c6052f917bb240ab561b4399d9
ColorFilter <johan@romantech.net>
2024-04-23 22:05:42 +0900

    :triangular_flag_on_post: level02-64065 튜플 (#16)

--- src/programmers/level-02/tests/level02_64065_튜플.test.js
+++ src/programmers/level-02/tests/level02_64065_튜플.test.js
@@ -1,6 +1,6 @@
-import { cases, solution } from '../level02_49994_방문길이';
+import { cases, solution } from '../level02_64065_튜플';
 
-describe('프로그래머스 | 레벨 2 | 방문길이', () => {
+describe('프로그래머스 | 레벨 2 | 튜플', () => {
   cases.forEach(({ input, output, desc }, i) => {
     test(`${i + 1}번 테스트 케이스
     설명: ${desc || '기본 테스트'}
