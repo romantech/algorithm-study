1e2e4c047884de0946c8a6a7ba4aa3cb92dd62e8
ColorFilter <johan@romantech.net>
2024-04-25 23:29:14 +0900

    테스트 케이스 추가

--- src/programmers/level-02/level02_68645_삼각_달팽이.js
+++ src/programmers/level-02/level02_68645_삼각_달팽이.js
@@ -28,7 +28,7 @@ const nextPos = ([x, y], dir) => {
   return [x + dx, y + dy];
 };
 
-function solution(n) {
+export function solution(n) {
   const { triangleArray, sum } = createTriangleArray(n);
   let pos = [0, 0];
   let num = 0;
@@ -55,7 +55,7 @@ function solution(n) {
   return triangleArray.flat();
 }
 
-const cases = [
+export const cases = [
   generateTestPair([4], [1, 2, 9, 3, 10, 8, 4, 5, 6, 7]),
   generateTestPair([5], [1, 2, 12, 3, 13, 11, 4, 14, 15, 10, 5, 6, 7, 8, 9]),
   generateTestPair(
@@ -63,5 +63,3 @@ const cases = [
     [1, 2, 15, 3, 16, 14, 4, 17, 21, 13, 5, 18, 19, 20, 12, 6, 7, 8, 9, 10, 11],
   ),
 ];
-
-console.log(solution(...cases[2].input));
---
+++ src/programmers/level-02/tests/level02_68645_삼각_달팽이.test.js
@@ -0,0 +1,13 @@
+import { cases, solution } from '../level02_68645_삼각_달팽이';
+
+describe('프로그래머스 | 레벨 2 | 삼각 달팽이', () => {
+  cases.forEach(({ input, output, desc }, i) => {
+    test(`${i + 1}번 테스트 케이스
+    설명: ${desc || '기본 테스트'}
+    입력값: ${JSON.stringify(input)}
+    예상값: ${output}
+    --------------------------------------------`, () => {
+      expect(solution(...input)).toEqual(output);
+    });
+  });
+});
