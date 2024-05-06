0d8d178c9131d7ee78e42866b9584e04946aba0b
ColorFilter <johan@romantech.net>
2024-05-06 15:08:07 +0900

    예상값 -> 기대값 문구 변경

--- src/programmers/level-02/tests/level02_43165_타겟_넘버.test.js
+++ src/programmers/level-02/tests/level02_43165_타겟_넘버.test.js
@@ -2,7 +2,7 @@ import { cases, solution } from '../level02_43165_타겟_넘버';
 
 describe('프로그래머스 - 레벨 02 - 타겟 넘버', () => {
   cases.forEach(({ input, output }) => {
-    test(`입력값: ${input} => 예상값: ${output}`, () => {
+    test(`입력값: ${input} => 기대값: ${output}`, () => {
       expect(solution(...input)).toEqual(output);
     });
   });
