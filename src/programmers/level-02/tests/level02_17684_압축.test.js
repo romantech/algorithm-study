ed2ea27987d3129212c9dd54a88eec06fad6841c
ColorFilter <johan@romantech.net>
2024-05-06 15:55:49 +0900

    예상값 -> 기대값 단어 변경

--- src/programmers/level-02/tests/level02_17684_압축.test.js
+++ src/programmers/level-02/tests/level02_17684_압축.test.js
@@ -1,12 +1,12 @@
-import { cases, solution, reference } from '../level02_17684_압축';
+import { cases, reference, solution } from '../level02_17684_압축';
 
 describe('프로그래머스 - 레벨 02 - 압축', () => {
   cases.forEach(({ input, output }) => {
-    test(`solution 함수 - 입력값: ${input} => 예상값: ${output}`, () => {
+    test(`solution 함수 - 입력값: ${input} => 기대값: ${output}`, () => {
       expect(solution(...input)).toEqual(output);
     });
 
-    test(`reference 함수 - 입력값: ${input} => 예상값: ${output}`, () => {
+    test(`reference 함수 - 입력값: ${input} => 기대값: ${output}`, () => {
       expect(reference(...input)).toEqual(output);
     });
   });
