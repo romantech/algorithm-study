b7d7ae3beed30ae323f9a83565047e1c0443aae2
ColorFilter <johan@romantech.net>
2024-05-06 17:43:51 +0900

    :pencil2: Fix typo, Update dependencies (#21)

--- src/programmers/level-01/tests/level01_161989_덧칠하기.test.js
+++ src/programmers/level-01/tests/level01_161989_덧칠하기.test.js
@@ -2,7 +2,7 @@ import { cases, solution } from '../level01_161989_덧칠하기.js';
 
 describe('프로그래머스 - 레벨 01 - 덧칠하기', () => {
   cases.forEach(({ input, output }) => {
-    test(`입력값: ${input} => 예상값: ${output}`, () => {
+    test(`입력값: ${input} => 기대값: ${output}`, () => {
       expect(solution(...input)).toEqual(output);
     });
   });
--- src/programmers/level-02/tests/level02_42584_주식가격.test.js
+++ src/programmers/level-02/tests/level02_42584_주식가격.test.js
@@ -1,12 +1,12 @@
-import { cases, solution, reference } from '../level02_42584_주식가격';
+import { cases, reference, solution } from '../level02_42584_주식가격';
 
 describe('프로그래머스 - 레벨 02 - 주식가격', () => {
   cases.forEach(({ input, output }) => {
-    test(`solution 함수 테스트 - 입력값: ${input} => 예상값: ${output}`, () => {
+    test(`solution 함수 테스트 - 입력값: ${input} => 기대값: ${output}`, () => {
       expect(solution(...input)).toEqual(output);
     });
 
-    test(`reference 함수 테스트 - 입력값: ${input} => 예상값: ${output}`, () => {
+    test(`reference 함수 테스트 - 입력값: ${input} => 기대값: ${output}`, () => {
       expect(reference(...input)).toEqual(output);
     });
   });
