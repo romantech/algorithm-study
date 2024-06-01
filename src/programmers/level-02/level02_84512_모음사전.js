6756f7542f8e0b7b3ac8de64d576505b028d9cc5
ColorFilter <johan@romantech.net>
2024-06-01 16:56:26 +0900

    :memo: 모음사전 문제 주석 수정

--- src/programmers/level-02/level02_84512_모음사전.js
+++ src/programmers/level-02/level02_84512_모음사전.js
@@ -14,7 +14,7 @@ import { generateTestPair } from '../../utils.js';
  * word : 길이 1 이상 5이하
  * word는 알파벳 대문자 A, E, I, O U로만 이루어져 있음
  *
- * [예시]
+ * [총 경우의 수]
  * A = 1, E = 2, I = 3, O = 4, U = 5로 가정했을 때
  * 1자리로 만들 수 있는 조합: 1, 2, 3, 4, 5 = 5가지
  * 2자리로 만들 수 있는 조합: 11, 12, ..., 51, 52, 53, 54, 55 = 5^5 25가지
@@ -47,7 +47,7 @@ export function solution(word) {
 }
 
 export function reference(word) {
-  // 각 자리의 가중치를 계산합니다.
+  // 각 자리의 가중치 계산
   // 1번째 자리 가중치 = 1 | 예: AAAAA(5) -> AAAAE (5+1) -> AAAAI (6+1)
   // 2번째 자리 가중치 = 6 = 1번째 자리 가중치 * 5(모음 개수) + 1   | 예: AAAA(4) -> AAAE (4+6*1) -> AAAI (4+6*2)
   // 3번째 자리 가중치 = 31 = 2번째 자리 가중치 * 5(모음 개수) + 1  | 예: AAA(3) -> AAE (3+31*1) -> AAI (3+31*2)
@@ -59,9 +59,8 @@ export function reference(word) {
   }, []); // [781, 156, 31, 6, 1]
 
   return [...word].reduce((totalWeight, char, i) => {
-    // 각 자리의 가중치를 계산하여 누적 가중치 계산
-    // 각 자리의 문자 인덱스에 해당하는 가중치에 해당 자리의 가중치를 곱하고,
-    // 누적 가중치에 더해준다. 마지막으로 +1을 더하여 각 자리수까지 포함시킵니다.
+    // 각 자리의 문자 인덱스(vowelIdxMap[char])를 가중치(weightPosition[i])에 곱하고,
+    // 누적 가중치(totalWeight)에 더해준다. 마지막으로 +1을 더해서 각 자리수까지 포함시킨다.
     return totalWeight + vowelIdxMap[char] * weightByPosition[i] + 1;
   }, 0);
 }
