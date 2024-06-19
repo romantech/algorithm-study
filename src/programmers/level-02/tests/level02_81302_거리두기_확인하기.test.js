ab468829865920096e6068cc7f35ca62a7ec2371
ColorFilter <johan@romantech.net>
2024-06-19 20:46:51 +0900

    :triangular_flag_on_post: level02-81302 거리두기 확인하기 (#35)

---
+++ src/programmers/level-02/tests/level02_81302_거리두기_확인하기.test.js
@@ -0,0 +1,21 @@
+import { cases, iterativeSolution, bfsSolution } from '../level02_81302_거리두기_확인하기';
+
+describe('프로그래머스 | 레벨 2 | 거리두기 확인하기', () => {
+	cases.forEach(({ input, output, desc }, i) => {
+		test(`${i + 1}번 테스트 케이스 iterativeSolution 함수
+    설명: ${desc || '기본 테스트'}
+    입력값: ${JSON.stringify(input)}
+    기대값: ${JSON.stringify(output)}
+    --------------------------------------------`, () => {
+			expect(iterativeSolution(...input)).toEqual(output);
+		});
+
+		test(`${i + 1}번 테스트 케이스 bfsSolution 함수
+    설명: ${desc || '기본 테스트'}
+    입력값: ${JSON.stringify(input)}
+    기대값: ${JSON.stringify(output)}
+    --------------------------------------------`, () => {
+			expect(bfsSolution(...input)).toEqual(output);
+		});
+	});
+});
